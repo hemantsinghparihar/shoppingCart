8657323a96ec466b755888061034c9b585154420
hemant <hemantsinghparihar13@gmail.com>
2024-11-12 16:53:34 +0530

    new build

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,122 +1,9 @@
-// import React, { useEffect,useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useNavigate, Link, useLocation } from 'react-router-dom';
-// import { setSearchResults, setSearchQuery, searchProductsRequest } from '../features/shoppingSlice';
-
-
-
-// const useDebounce=(value,delay)=>{
-//   const [debouncedValue,setDebouncedValue]=useState(value)
-//   useEffect(()=>{
-//     const handler = setTimeout(() => {
-//       setDebouncedValue(value);
-//     }, delay);
-
-//     // Cleanup the timer if the value changes before the delay finishes
-//     return () => {
-//       clearTimeout(handler);
-//     };
-//   },[value,delay])
-  
-  
-//   return debouncedValue
-// }
-
-// function Header() {  
-//   const navigate = useNavigate();
-//   const location = useLocation();
-//   const dispatch = useDispatch();
-  
-//   // Select search query and debounced query for search functionality
-//   const searchQuery = useSelector((state) => state.shop.searchQuery);
-//   const debouncedQuery = useDebounce(searchQuery, 800);
-
-//   // Trigger search action if there is a debounced query
-//   useEffect(() => {
-//     if (debouncedQuery) {
-//       dispatch(searchProductsRequest(debouncedQuery));
-//     }
-//   }, [debouncedQuery, dispatch]);
-
-//   // Handle logout functionality
-//   const handleLogout = () => {
-//     localStorage.clear();
-//     navigate('/', { replace: true });
-//   };
-
-//   // Update search query in Redux store on input change
-//   const handleSearchQuery = (e) => {
-//     dispatch(setSearchQuery(e.target.value));
-//   };
-
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
-//       {/* Left - Logo */}
-//       <Link to="/" className="navbar-brand">
-//         MyApp
-//       </Link>
-
-//       {/* Bootstrap Hamburger Icon for Mobile */}
-//       <button
-//         className="navbar-toggler"
-//         type="button"
-//         data-bs-toggle="collapse"
-//         data-bs-target="#navbarNav"
-//         aria-controls="navbarNav"
-//         aria-expanded="false"
-//         aria-label="Toggle navigation"
-//       >
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-
-//       {/* Collapsible Navbar */}
-//       <div className="collapse navbar-collapse" id="navbarNav">
-//         {/* Search bar - only displayed when not on the cart page */}
-//         {location.pathname !== '/cart' && (
-//           <form className="d-flex mx-auto my-2 my-lg-0" style={{ maxWidth: '400px', width: '100%' }}>
-//             <input
-//               className="form-control me-2"
-//               type="search"
-//               placeholder="Search..."
-//               aria-label="Search"
-//               value={searchQuery}
-//               onChange={handleSearchQuery}
-//             />
-//           </form>
-//         )}
-
-//         {/* Right - Links and Buttons */}
-//         <ul className="navbar-nav ms-auto d-flex align-items-center">
-//           <li className="nav-item">
-//             <Link to="/cart" className="nav-link">
-//               <i className="ri-shopping-cart-2-fill"></i>
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <i className="ri-user-3-fill mx-2"></i>
-//           </li>
-//           <li className="nav-item">
-//             <button onClick={handleLogout} className="btn btn-primary btn-sm ms-2">
-//               Logout
-//             </button>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Header;
-
-
-
-
-
 import React, {useState,useEffect} from 'react'
 import { useSelector,useDispatch } from 'react-redux';
 import { useNavigate,Link,useLocation } from 'react-router-dom'
 import { setSearchResults,setSearchQuery,searchProductsRequest } from '../features/shoppingSlice';
 import prodServices from '../services/productsApi';
+
 
 
 const useDebounce=(value,delay)=>{
@@ -207,7 +94,7 @@ function Header() {
 
         {
           location.pathname!=='/cart' && 
-          <form className="d-flex mx-auto " style={{ maxWidth: '400px', width: '100%' }}>
+          <form className="d-none d-md-flex mx-auto search-bar" style={{ maxWidth: '400px', width: '100%' }}>
             <input
               className="form-control me-2 "
               type="search"
